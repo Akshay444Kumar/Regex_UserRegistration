3edc3fc438f0b860a8fc7ea05415493bc0d7e366
Akshay444Kumar <aryakshay444@gmail.com>
2023-04-07 19:13:45 +0530

    ADDED: Welcome to User Registration Message

---
+++ src/com/user_registration_regex/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.user_registration_regex;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program");
+    }
+}
