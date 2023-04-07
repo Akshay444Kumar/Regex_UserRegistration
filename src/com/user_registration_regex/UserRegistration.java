76543e2f684f7669bc33e908af554f5e1a865464
Akshay444Kumar <aryakshay444@gmail.com>
2023-04-07 19:22:07 +0530

    ADDED: Program to check valid First Name using Regex Patter

--- src/com/user_registration_regex/UserRegistration.java
+++ src/com/user_registration_regex/UserRegistration.java
@@ -1,8 +1,21 @@
 package com.user_registration_regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Program");
+        Scanner scan = new Scanner(System.in);
+        // Method To Check First Name With Regex Start With Cap And 3 Min. Character
+        System.out.println("Enter the first Name: ");
+        String firstName = scan.nextLine();
+
+        if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
+            System.out.println("First name is valid");
+        } else {
+            System.out.println("Entered first name is invalid");
+        }
+        scan.close();
     }
 }
